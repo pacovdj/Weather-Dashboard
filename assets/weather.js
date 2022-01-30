8022f2b707165acd79ef771f2de869a232999d38
Mona <you@example.com>
2022-01-30 18:13:01 -0500

    add datebuilder

--- assets/weather.js
+++ assets/weather.js
@@ -25,4 +25,25 @@ function displayResults (weather) {
     console.log(weather);
     let city = document.querySelector('.location .city');
     city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+
+let now = new Date();
+let date = document.querySelector('.location .date');
+date.innterText = dateBuilder (now);
+
+}
+
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", 
+    "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days [d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+
+
 }
